24aac9a11e7cee691640b4edac129ff9eaa0f1cd
Rafael lima <rafaellrf09@gmail.com>
2020-10-06 22:31:23 -0300

    fix

--- src/controller/RecipeController.js
+++ src/controller/RecipeController.js
@@ -3,16 +3,20 @@ const recipeService = require('../services/RecipeService');
 
 class RecipeController {
   async index(req, res) {
+    try {
     const { i } = req.query;
 
     const arrayParameter = i.split(", ");
 
     if(arrayParameter.length > 3 ) return res.status(400).json({error: "More than 3 ingredients were passed"})
-    try {
-      const results = await recipeService.search(arrayParameter);
-      return res.json(results);
+
+    const results = await recipeService.search(arrayParameter);
+
+    return res.json(results);
     } catch (error) {
+
       return res.status(500).json({error: "Internal server error."})
+
     }
   }
 }
