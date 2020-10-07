682a196aed2eb046817032ce91b495024777a32d
Rafael lima <rafaellrf09@gmail.com>
2020-10-07 12:17:56 -0300

    modularizando index e app js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const routes = require('./routes');
-
-
-const app = express();
-
-
-app.use(bodyParser.json());
-
-app.use(routes);
+const app = require('./app');
 
 app.listen(process.env.APP_PORT);
 console.log(`Sevidor iniciado na porta ${process.env.APP_PORT}`);
